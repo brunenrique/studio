3c58bba992e5dc499c993282839e39c65444e1a7
brunenrique <93277400+brunenrique@users.noreply.github.com>
2025-06-06 19:40:02 -0300

    Revert "Replace Image with CustomImage wrapper"

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
   allowedDevOrigins: [
