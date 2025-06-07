4a43ad605c9a7faebbecb2a670bd6f729eb6a65a
brunenrique <93277400+brunenrique@users.noreply.github.com>
2025-06-07 02:42:05 -0300

    Enable coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
