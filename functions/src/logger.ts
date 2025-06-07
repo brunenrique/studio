ae4fa85cf7c1cb81aafe124dd2680b119d53bb79
brunenrique <93277400+brunenrique@users.noreply.github.com>
2025-06-07 03:12:24 -0300

    Refine logger typing

--- functions/src/logger.ts
+++ functions/src/logger.ts
@@ -1,3 +1,8 @@
-export function logSendResult(channel: string, patientId: string, status: string, info: any) {
+export function logSendResult(
+  channel: string,
+  patientId: string,
+  status: 'success' | 'error',
+  info: unknown,
+) {
   console.info(JSON.stringify({ channel, patientId, status, info }));
 }
